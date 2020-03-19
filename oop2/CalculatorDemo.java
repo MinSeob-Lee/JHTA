205c34eaf73a16257e16f30a5252410a07c210b5
MinSeob-Lee <61997830+MinSeob-Lee@users.noreply.github.com>
2020-03-19 17:54:51 +0900

    Add files via upload

---
+++ oop2/CalculatorDemo.java
@@ -0,0 +1,13 @@
+package oop2;
+
+public class CalculatorDemo {
+
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		
+		// int result = calculator.plus(6, 8);
+		// double result2 = calculator.plus(6.9, 8.1);
+		// long result3 = calculator.plus(6000L, 800000L);
+		int result4 = calculator.plus(6, 8, 7);
+	}
+}
---
+++ oop2/MyUtils.java
@@ -0,0 +1,11 @@
+package oop2;
+
+import java.text.DecimalFormat;
+
+public class MyUtils {
+
+	public static String numberwithComma(long number) {
+		DecimalFormat df = new DecimalFormat("#,###");
+		return df.format(number);
+	}
+}

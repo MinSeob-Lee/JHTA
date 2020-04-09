c7cbe0af0f13ad546ae14d1c77ff5a8eafaef9ee
MinSeob-Lee <61997830+MinSeob-Lee@users.noreply.github.com>
2020-04-09 11:57:34 +0900

    Add files via upload

--- collection/src/collection/recruit/RecruitApp.java
+++ collection/src/collection/recruit/RecruitApp.java
@@ -12,23 +12,25 @@ public class RecruitApp {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		RecruitmentService service = new RecruitmentService();
-		
+
 		while (true) {
 			System.out.println("=========================================");
 			System.out.println("1.구직자  2.인사담당자  0.종료");
 			System.out.println("=========================================");
-			
+
 			System.out.print("메뉴를 선택하세요: ");
 			int menuNo = scanner.nextInt();
-			
+
 			if (menuNo == 1) {
-				System.out.println("==========================================================================");
-				System.out.println("1.이력서등록 2.경력사항등록 3.채용상태확인 4.이력서수정 5.구직신청 삭제");
-				System.out.println("==========================================================================");
-				
+				System.out.println(
+						"=========================================================================================");
+				System.out.println("1.이력서등록 2.경력사항등록 3.채용상태확인 4.이력서수정 5.구직신청 삭제 6.경력사항 수정 7.구직신청 현황 조회");
+				System.out.println(
+						"=========================================================================================");
+
 				System.out.print("구직자 메뉴를 선택하세요 : ");
 				int empMenuNo = scanner.nextInt();
-				
+
 				if (empMenuNo == 1) {
 					// 이름, 전화번호, 지원부서를 입력받는다.
 					System.out.println("[이력서 등록]");
@@ -38,12 +40,12 @@ public class RecruitApp {
 					String tel = scanner.next();
 					System.out.print("지원부서를 입력해주세요 : ");
 					String dept = scanner.next();
-					
+
 					Resume resume = new Resume();
 					resume.setName(name);
 					resume.setTel(tel);
 					resume.setDept(dept);
-					
+
 					service.addNewResume(resume);
 				} else if (empMenuNo == 2) {
 					// 구직신청번호, 재직회사명, 근무부서, 직위, 근무년수를 입력받는다.
@@ -58,20 +60,20 @@ public class RecruitApp {
 					String position = scanner.next();
 					System.out.print("근무년수를 입력해주세요 : ");
 					int year = scanner.nextInt();
-					
+
 					Career career = new Career();
 					career.setCompanyName(companyName);
 					career.setDept(dept);
 					career.setPosition(position);
 					career.setYear(year);
-					
+
 					service.addCareer(recruitmentNo, career);
 				} else if (empMenuNo == 3) {
 					// 구직신청번호를 입력받는다.
 					System.out.println("[채용 상태 확인]");
 					System.out.print("구직신청번호를 입력해주세요 : ");
 					int recruitmentNo = scanner.nextInt();
-					
+
 					service.printRecruitmentDetail(recruitmentNo);
 				} else if (empMenuNo == 4) {
 					// 구직신청번호, 이름, 전화번호, 지원부서를 입력받는다.
@@ -84,41 +86,102 @@ public class RecruitApp {
 					String tel = scanner.next();
 					System.out.print("지원부서를 입력해주세요 : ");
 					String dept = scanner.next();
-					
+
 					Resume resume = new Resume();
 					resume.setName(name);
 					resume.setTel(tel);
 					resume.setDept(dept);
-					
+
 					service.updateResume(recruitmentNo, resume);
 				} else if (empMenuNo == 5) {
 					// 구직신청번호를 입력받는다.
 					System.out.println("[구직신청 삭제]");
 					System.out.print("구직신청번호를 입력해주세요 : ");
 					int recruitmentNo = scanner.nextInt();
-					
+
 					service.deleteRecruitment(recruitmentNo);
-				} 
-				
+				} else if (empMenuNo == 6) {
+					System.out.println("[경력사항 수정]");
+					System.out.println("====================================");
+					System.out.println("1.경력사항 수정   2.경력사항 삭제");
+					System.out.println("====================================");
+					System.out.print("경력사항 수정 메뉴를 선택하세요 : ");
+					int careerMenuNo = scanner.nextInt();
+
+					if (careerMenuNo == 1) {
+						System.out.println("[경력사항 수정]");
+						System.out.print("구직신청번호를 입력해주세요 : ");
+						int recruitmentNo = scanner.nextInt();
+						System.out.print("재직회사명을 입력해주세요 : ");
+						String companyName = scanner.next();
+						System.out.print("수정할 근무부서를 입력해주세요 : ");
+						String dept = scanner.next();
+						System.out.print("수정할 직위를 입력해주세요 : ");
+						String position = scanner.next();
+						System.out.print("수정할 근무년수를 입력해주세요 : ");
+						int year = scanner.nextInt();
+
+						Career career = new Career();
+						career.setDept(dept);
+						career.setPosition(position);
+						career.setYear(year);
+
+						service.updateCareer(recruitmentNo, companyName, career);
+					} else if (careerMenuNo == 2) {
+						System.out.println("[경력사항 삭제]");
+						System.out.print("구직신청번호를 입력해주세요 : ");
+						int recruitmentNo = scanner.nextInt();
+						System.out.print("재직회사명을 입력해주세요 : ");
+						String companyName = scanner.next();
+
+						service.deleteCareer(recruitmentNo, companyName);
+					}
+				} else if (empMenuNo == 7) {
+					System.out.println("[구직신청 현황 조회]");
+					System.out.println("============================================================");
+					System.out.println("1.지원자수 조회   2.중복 지원자수 조회   3.중복 지원자 평균 근무기간 조회");
+					System.out.println("============================================================");
+					System.out.print("구직신청 현황 조회 메뉴를 선택하세요 : ");
+					int recruitmentStatusNo = scanner.nextInt();
+
+					if (recruitmentStatusNo == 1) {
+						System.out.println("[지원자수 조회]");
+
+						service.findRecruitmentNumber();
+					} else if (recruitmentStatusNo == 2) {
+						System.out.println("[중복 지원자수 조회]");
+						System.out.print("구직신청번호를 입력해주세요 : ");
+						int recruitmentNo = scanner.nextInt();
+
+						service.findRecruitmentDeptNumber(recruitmentNo);
+					} else if (recruitmentStatusNo == 3) {
+						System.out.println("[중복 지원자 평균 근무기간 조회]");
+						System.out.print("구직신청번호를 입력해주세요 : ");
+						int recruitmentNo = scanner.nextInt();
+
+						service.findRecruitmentAvgYear(recruitmentNo);
+					}
+				}
+
 			} else if (menuNo == 2) {
 				System.out.println("==========================================================================");
-				System.out.println("1.채용신청서 조회  2.채용신청서 상세조회  3.채용신청서 심사");
+				System.out.println("1.채용신청서 조회  2.채용신청서 상세조회  3.채용신청서 심사  4.전체 구직신청조회  0.종료");
 				System.out.println("==========================================================================");
-				
+
 				System.out.print("구직자 메뉴를 선택하세요 : ");
 				int empMenuNo = scanner.nextInt();
-				
+
 				if (empMenuNo == 1) {
 					// 입력값 없음
 					System.out.println("[채용신청서 조회]");
-					
+
 					service.printAllRecruitment();
 				} else if (empMenuNo == 2) {
 					// 구직신청번호를 입력받는다.
 					System.out.println("[채용신청서 상세조회]");
 					System.out.print("구직신청번호를 입력해주세요 : ");
 					int recruitmentNo = scanner.nextInt();
-					
+
 					service.printRecruitmentDetail(recruitmentNo);
 				} else if (empMenuNo == 3) {
 					// 구직신청번호와 합격불합격여부를 Y/N으로 입력받는다.
@@ -127,20 +190,35 @@ public class RecruitApp {
 					int recruitmentNo = scanner.nextInt();
 					System.out.print("합격/불합격 여부를 입력해주세요(Y/N) : ");
 					String passed = scanner.next();
-					
+
 					boolean isPassed = false;
-					if(passed.equals("Y")) {
+					if (passed.equals("Y")) {
 						isPassed = true;
 					}
-					
+
 					service.checkRecruitment(recruitmentNo, isPassed);
-				} 
-				
-			} else if (menuNo == 0) {
-				System.out.println("### 프로그램을 종료합니다.");
-				break;
+				} else if (empMenuNo == 4) {
+					System.out.println("[전체 구직신청 조회]");
+					System.out.println("=================================");
+					System.out.println("1.이름순 정렬   2.근무경력순 정렬");
+					System.out.println("=================================");
+					System.out.print("정렬 메뉴를 선택하세요 : ");
+					int sortMenu = scanner.nextInt();
+
+					if (sortMenu == 1) {
+						System.out.println("[이름순 정렬]");
+
+						service.printAllRecruitmentSortedByName();
+					} else if (sortMenu == 2) {
+						System.out.println("[근무경력순 정렬]");
+
+						service.printAllRecruitmentSortedByYear();
+					}
+				} else if (menuNo == 0) {
+					System.out.println("### 프로그램을 종료합니다.");
+					break;
+				}
 			}
-			System.out.println();
 		}
 		scanner.close();
 	}
--- collection/src/collection/recruit/service/RecruitmentService.java
+++ collection/src/collection/recruit/service/RecruitmentService.java
@@ -1,6 +1,9 @@
 package collection.recruit.service;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 
 import collection.recruit.repo.RecruitmentRepository;
 import collection.recruit.vo.Career;
@@ -29,11 +32,11 @@ public class RecruitmentService {
 		// 레포지토리에서 구직신청번호에 해당하는 구직신청객체를 조회한다.
 		// 구직신청객체의 경력사항에 전달받은 경력사항객체를 추가한다.
 		Recruitment recruitment = repo.getRecruitmentByNo(recruitmentNo);
-		
-		if(recruitment == null) {
+
+		if (recruitment == null) {
 			return;
 		}
-		
+
 		recruitment.addCareer(career);
 	}
 
@@ -50,26 +53,26 @@ public class RecruitmentService {
 		System.out.println("이    름 : " + resume.getName());
 		System.out.println("전화번호 : " + resume.getTel());
 		System.out.println("희망부서 : " + resume.getDept());
-		
+
 		ArrayList<Career> careers = recruitment.getCareers();
-		for(Career career : careers) {
+		for (Career career : careers) {
 			System.out.println("회 사 명 : " + career.getCompanyName());
 			System.out.println("부 서 명 : " + career.getDept());
 			System.out.println("직    급 : " + career.getPosition());
 			System.out.println("근무연수 : " + career.getYear());
+			System.out.println();
 		}
-		System.out.println("경력정보 조회 완료");
-		
+
 		String passed = "";
-		
-		if(recruitment.isPassed()) {
+
+		if (recruitment.isPassed()) {
 			passed = "합격";
 		} else {
 			passed = "불합격";
 		}
-		
+
 		System.out.println("합격여부 : " + passed);
-		
+
 	}
 
 	// 이력서 수정 기능
@@ -77,7 +80,7 @@ public class RecruitmentService {
 		// 레포지토리에서 구직신청번호에 해당하는 구직신청객체를 조회한다.
 		// 구직신청객체의 이력서정보를 전달받은 이력서 정보로 교체한다.
 		Recruitment recruitment = repo.getRecruitmentByNo(recruitmentNo);
-		recruitment.setResume(resume);	
+		recruitment.setResume(resume);
 	}
 
 	// 구직신청 삭제하기
@@ -93,7 +96,7 @@ public class RecruitmentService {
 		System.out.println("-------------------------------------------------");
 		System.out.println("구직신청번호\t이름\t경력여부\t합격여부");
 		System.out.println("-------------------------------------------------");
-		for(Recruitment recruit : recruitments) {
+		for (Recruitment recruit : recruitments) {
 			Resume resume = recruit.getResume();
 			String isCareer = "";
 			if (recruit.getCareers().isEmpty()) {
@@ -102,7 +105,7 @@ public class RecruitmentService {
 				isCareer = "있음";
 			}
 			System.out.print(recruit.getNo() + "\t");
-			System.out.print(resume.getName()+ "\t");
+			System.out.print(resume.getName() + "\t");
 			System.out.print(isCareer + "\t");
 			System.out.println(recruit.isPassed());
 		}
@@ -114,15 +117,189 @@ public class RecruitmentService {
 		// 합격/불합격여부를 결정한다.
 		Recruitment recruitment = repo.getRecruitmentByNo(recruitmentNo);
 		recruitment.setPassed(passed);
-		
+
 		String pass = "";
-		
-		if(recruitment.isPassed()) {
+
+		if (recruitment.isPassed()) {
 			pass = "합격";
 		} else {
 			pass = "불합격";
 		}
-		
 		System.out.println("합격여부 : " + pass);
 	}
+
+	// 경력사항 수정하기
+	public void updateCareer(int recruitmentNo, String companyName, Career career) {
+		Recruitment recruitment = repo.getRecruitmentByNo(recruitmentNo);
+
+		if (recruitment == null) {
+			System.out.println("일치하는 구직정보가 없습니다.");
+			return;
+		}
+
+		ArrayList<Career> careers = recruitment.getCareers();
+		Iterator<Career> itr = careers.iterator();
+		
+		while(itr.hasNext()) {
+			Career car = itr.next();
+			
+			if (!(companyName.equals(car.getCompanyName()))) {
+				System.out.println("일치하는 회사명이 없습니다.");
+				return;
+			}
+			
+			car.setDept(career.getDept());
+			car.setPosition(career.getPosition());
+			car.setYear(career.getYear());
+			
+			System.out.println("경력사항 수정이 완료되었습니다.");
+		}
+	}
+
+	// 경력사항 삭제하기
+	public void deleteCareer(int recruitmentNo, String companyName) {
+		Recruitment recruitment = repo.getRecruitmentByNo(recruitmentNo);
+
+		if (recruitment == null) {
+			System.out.println("일치하는 구직정보가 없습니다.");
+			return;
+		}
+
+		ArrayList<Career> careers = recruitment.getCareers();
+		Iterator<Career> itr = careers.iterator();
+		
+		while(itr.hasNext()) {
+			Career car = itr.next();
+			
+			if (!(companyName.equals(car.getCompanyName()))) {
+				System.out.println("일치하는 회사명이 없습니다.");
+				return;
+			}
+			
+			itr.remove();
+			
+			System.out.println("경력사항 삭제가 완료되었습니다.");
+		}
+	}
+
+	// 지원자 수 조회하기
+	public void findRecruitmentNumber() {
+		ArrayList<Recruitment> recruitments = repo.getAllRecruitments();
+
+		System.out.println("지원자 수는 " + recruitments.size() + "명입니다.");
+	}
+
+	// 자신과 같은 부서에 지원한 사람들의 숫자
+	public void findRecruitmentDeptNumber(int recruitmentNo) {
+		Recruitment recruitment = repo.getRecruitmentByNo(recruitmentNo);
+		ArrayList<Recruitment> recruitments = repo.getAllRecruitments();
+		Resume resume = recruitment.getResume();
+		int count = 0;
+		
+		for(Recruitment recruitDept : recruitments) {
+			if(resume.getDept().equals(recruitDept.getResume().getDept())) {
+				count++;
+			}			
+		}		
+		System.out.println("귀하와 같은 부서에 지원한 사람들의 숫자는 귀하를 포함해 총 " + count + "명입니다.");
+	}
+	
+	// 자신과 같은 부서에 지원한 사람들의 평균 근무기간
+	public void findRecruitmentAvgYear(int recruitmentNo) {
+		Recruitment recruitment = repo.getRecruitmentByNo(recruitmentNo);
+		ArrayList<Recruitment> recruitments = repo.getAllRecruitments();
+		Resume resume = recruitment.getResume();
+		int count = 0;
+		int year = 0;
+		
+		for(Recruitment recruitDept : recruitments) {
+			if(resume.getDept().equals(recruitDept.getResume().getDept())) {
+				count++;
+				
+				ArrayList<Career> careers = recruitDept.getCareers();
+				for(Career car : careers) {
+					year += car.getYear();
+				}
+			}			
+		}
+		int avgYear = (int)(year / count);
+		System.out.println("귀하와 같은 부서에 지원한 사람들의 평균 근무기간은 " + avgYear + "년입니다.");
+	}
+	
+	// 전체 구직신청조회 + 이름순 정렬하기
+	public void printAllRecruitmentSortedByName() {
+		// 레포지토리에서 모든 구직신청 정보를 조회해서 화면에 출력한다.
+		ArrayList<Recruitment> recruitments = repo.getAllRecruitments();
+		System.out.println("-------------------------------------------------");
+		System.out.println("구직신청번호\t이름\t경력여부\t합격여부");
+		System.out.println("-------------------------------------------------");
+		
+		Collections.sort(recruitments, (r1, r2) -> r1.getResume().getName().compareTo(r2.getResume().getName()));
+		
+		for (Recruitment recruit : recruitments) {
+			Resume resume = recruit.getResume();
+			String isCareer = "";
+			if (recruit.getCareers().isEmpty()) {
+				isCareer = "없음";
+			} else {
+				isCareer = "있음";
+			}
+			System.out.print(recruit.getNo() + "\t");
+			System.out.print(resume.getName() + "\t");
+			System.out.print(isCareer + "\t");
+			System.out.println(recruit.isPassed());
+		}
+	}
+	
+	// 전체 구직신청조회 + 근무경력순(내림차순) 정렬하기
+		public void printAllRecruitmentSortedByYear() {
+			// 레포지토리에서 모든 구직신청 정보를 조회해서 화면에 출력한다.
+			ArrayList<Recruitment> recruitments = repo.getAllRecruitments();
+			int yearSum = 0;
+			
+			Collections.sort(recruitments, new Comparator<Recruitment>() {
+				@Override
+				public int compare(Recruitment o1, Recruitment o2) {
+					ArrayList<Career> careers1 = o1.getCareers();
+					int o1Year = 0;
+					for(Career car : careers1) {
+						o1Year += car.getYear();
+					}
+					
+					ArrayList<Career> careers2 = o2.getCareers();
+					int o2Year = 0;
+					for(Career car : careers2) {
+						o2Year += car.getYear();
+					}
+					
+					return (o1Year - o2Year) * -1;
+				}
+			});
+			
+			for (Recruitment recruit : recruitments) {
+				ArrayList<Career> careers = recruit.getCareers();	
+				Resume resume = recruit.getResume();
+				String isCareer = "";
+				if (recruit.getCareers().isEmpty()) {
+					isCareer = "없음";
+				} else {
+					isCareer = "있음";
+				}
+				System.out.println("-------------------------------------------------");
+				System.out.println("구직신청번호\t이름\t경력여부\t합격여부");
+				System.out.println("-------------------------------------------------");
+				System.out.print(recruit.getNo() + "\t");
+				System.out.print(resume.getName() + "\t");
+				System.out.print(isCareer + "\t");
+				System.out.println(recruit.isPassed());
+				System.out.println("[경력 세부사항]");
+				for(Career car : careers) {
+					System.out.println("재직회사 : " + car.getCompanyName());
+					System.out.println("부서 : " + car.getDept());
+					System.out.println("직급 : " + car.getPosition());
+					System.out.println("근무기간 : " + car.getYear());
+					System.out.println();
+				}
+			}
+		}
 }
